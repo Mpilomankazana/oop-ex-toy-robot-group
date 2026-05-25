8bf5365bf445e466ba3923deaf2b5b2c44da4fa9
MpiloMankazana <Mpilomankazana601@gmail.com>
2026-05-25 17:30:15 +0200

    Movement Test:
    - Direction moves correctly
    
    -Robot stays still at zero steps

---
+++ src/test/java/za/co/wethinkcode/robots/world/MovementTest.java
@@ -0,0 +1,68 @@
+package za.co.wethinkcode.robots.world;
+
+import org.junit.jupiter.api.Test;
+import za.co.wethinkcode.robots.robot.Direction;
+import za.co.wethinkcode.robots.robot.Robot;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MovementTest {
+
+    @Test
+    void testRobotMovesForwardCorrectly() {
+
+        Robot robot = new Robot("HAL");
+
+        robot.forward(3);
+
+        assertEquals(0, robot.getX());
+        assertEquals(3, robot.getY());
+    }
+
+    @Test
+    void testBackMovesReverseNoDirectionChange() {
+
+        Robot robot = new Robot("HAL");
+
+        robot.back(2);
+
+        assertEquals(0, robot.getX());
+        assertEquals(-2, robot.getY());
+
+        assertEquals(Direction.NORTH,
+                robot.getDirection());
+    }
+
+    @Test
+    void testTurnRightChangesDirection() {
+
+        Robot robot = new Robot("HAL");
+
+        robot.turnRight();
+
+        assertEquals(Direction.EAST,
+                robot.getDirection());
+    }
+
+    @Test
+    void testTurnLeftChangesDirection() {
+
+        Robot robot = new Robot("HAL");
+
+        robot.turnLeft();
+
+        assertEquals(Direction.WEST,
+                robot.getDirection());
+    }
+
+    @Test
+    void testRobotDoesNotMoveWhenZeroSteps() {
+
+        Robot robot = new Robot("HAL");
+
+        robot.forward(0);
+
+        assertEquals(0, robot.getX());
+        assertEquals(0, robot.getY());
+    }
+}
