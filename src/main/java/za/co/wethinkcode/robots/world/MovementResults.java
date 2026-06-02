cd56a92b047bc99f3de8caee1b90aaa025f48472
MpiloMankazana <Mpilomankazana601@gmail.com>
2026-06-02 13:25:52 +0200

    added MovementResults

---
+++ src/main/java/za/co/wethinkcode/robots/world/MovementResults.java
@@ -0,0 +1,22 @@
+package za.co.wethinkcode.robots.world;
+//import za.co.wethinkcode.robots.robot.Robot;
+
+public class MovementResults {
+
+    private final boolean success;
+    private final String message;
+
+    public MovementResults(boolean success, String message){
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean isSuccess(){
+        return success;
+    }
+
+    public String getMessege(){
+        return message;
+    }
+
+}
