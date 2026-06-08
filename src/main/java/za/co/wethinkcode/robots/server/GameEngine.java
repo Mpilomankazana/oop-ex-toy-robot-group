8d580057f89ee0eb5068be2faf0aab43d402a553
MpiloMankazana <Mpilomankazana601@gmail.com>
2026-06-08 11:22:28 +0200

    added launch method to game engine with random placement loop

--- src/main/java/za/co/wethinkcode/robots/server/GameEngine.java
+++ src/main/java/za/co/wethinkcode/robots/server/GameEngine.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class GameEngine {
 
@@ -174,6 +175,24 @@ public class GameEngine {
             this.distance = distance;
             this.shotleft = shotleft;
         }
+    }
+
+    public Robot launch(String name, World world){
+       Random random = new Random();
+       int halfWidth = world.getWidth() / 2;
+       int halfHeight = world.getHeight() / 2;
+       int x, y;
+       int attempts = 0;
+
+       do{
+           x = random.nextInt(world.getWidth()) - halfWidth;
+           y = random.nextInt(world.getHeight()) - halfHeight;
+           attempts++;
+           if (attempts > 1000) return null;
+       } while (world.isBlocked(x,y));
+       Robot robot = new Robot(name , x ,y);
+       world.addRobot(robot);
+       return robot;
     }
 
 }
