d103f9164886be085c8ad1f0c65ee02acbbfb6a7
MpiloMankazana <Mpilomankazana601@gmail.com>
2026-06-11 14:18:02 +0200

    added the displayRepairResponse method

--- src/main/java/za/co/wethinkcode/robots/client/Client.java
+++ src/main/java/za/co/wethinkcode/robots/client/Client.java
@@ -121,7 +121,7 @@ public class Client {
                 case "fire":
                 case "look":
                 case "state":
-                case"repair":
+                case "repair":
 
                     json = "{\"robot\":\"" + robotName +
                             "\",\"command\":\"" + command +
@@ -146,14 +146,14 @@ public class Client {
 
             } else if (command.equals("forward") || command.equals("back")) {
                 displayMovementResponse(response);
-                
+
             } else if (command.equals("turn")) {
                 displayTurnResponse(response);
 
             } else if (command.equals("fire")) {
                 displayFireResponse(response);
 
-            }else if (command.equals("repair")) {
+            } else if (command.equals("repair")) {
                 displayRepairResponse(response);
 
             } else {
@@ -210,7 +210,6 @@ public class Client {
             System.out.println("unexpected response from java");
         }
     }
-
 
 
     private static void displayStateResponse(String response) {
@@ -279,6 +278,21 @@ public class Client {
                     System.out.println("Miss, no robot in range.");
                 }
                 System.out.println(" shots remaining: " + shotsLeft);
+            } else {
+                System.out.println(response);
+            }
+        } catch (Exception e) {
+            System.out.println(response);
+        }
+    }
+
+    private static void displayRepairResponse(String response) {
+        try {
+            JsonNode node = mapper.readTree(response);
+            JsonNode state = node.get("state");
+            if (state != null) {
+                System.out.println("Robot repaired");
+                System.out.println(" Shields: " + state.get("shields").asInt());
             } else {
                 System.out.println(response);
             }
