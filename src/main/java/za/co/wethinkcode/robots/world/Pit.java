19944974bf8a7a5550883a686a4db627b2e0a760
MpiloMankazana <Mpilomankazana601@gmail.com>
2026-06-20 11:11:42 +0200

    Add Pit class extending Obstacle

---
+++ src/main/java/za/co/wethinkcode/robots/world/Pit.java
@@ -0,0 +1,13 @@
+package za.co.wethinkcode.robots.world;
+
+/**
+ * Represents a Pit obstacle - a rectangular hole in the terrain.
+ * Robots cannot move Though or fire through a pit.
+ */
+
+public class Pit extends Obstacle {
+
+    public Pit (int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+}
