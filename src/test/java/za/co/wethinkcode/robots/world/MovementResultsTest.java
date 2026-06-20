ef59c84d4f8318e6eb4ede3062e6d081841a49be
MpiloMankazana <Mpilomankazana601@gmail.com>
2026-06-20 10:57:55 +0200

    Movement Results Tests:
    - Should store success flag
    
    - Should store message

---
+++ src/test/java/za/co/wethinkcode/robots/world/MovementResultsTest.java
@@ -0,0 +1,23 @@
+package za.co.wethinkcode.robots.world;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MovementResultsTest {
+
+    @Test
+    void shouldStoreSuccessFlag(){
+
+        MovementResults result =
+                new MovementResults(true, "Moved");
+    }
+
+    @Test
+    void shouldStoreMessage(){
+        MovementResults result =
+                new MovementResults(false, "Blocked");
+
+        assertEquals("Blocked", result.getMessege());
+    }
+}
