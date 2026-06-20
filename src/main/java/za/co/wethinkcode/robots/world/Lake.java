005f0d50b00d8ac77a6d80d2cde807f278cd6e90
MpiloMankazana <Mpilomankazana601@gmail.com>
2026-06-20 11:01:45 +0200

    Add getType method to Lake returning LAKE

--- src/main/java/za/co/wethinkcode/robots/world/Lake.java
+++ src/main/java/za/co/wethinkcode/robots/world/Lake.java
@@ -1,5 +1,7 @@
 package za.co.wethinkcode.robots.world;
 
+import java.util.SplittableRandom;
+
 /**
  * Represents a Lake obstacle - a rectangular water terrain feature.
  * Robots cannot move though or fire through a lake.
@@ -9,5 +11,10 @@ public class Lake extends Obstacle {
 
     public Lake (int x, int y, int width, int height) {
         super(x, y, width, height);
+    }
+
+    @Override
+    public String getType() {
+        return "LAKE";
     }
 }
