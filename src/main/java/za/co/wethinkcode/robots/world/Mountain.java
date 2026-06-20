efd4ce0eeb8846e6cc08099fa033b598b7c72ce9
MpiloMankazana <Mpilomankazana601@gmail.com>
2026-06-20 10:37:39 +0200

    Add Mountain class extending Obstacle

---
+++ src/main/java/za/co/wethinkcode/robots/world/Mountain.java
@@ -0,0 +1,16 @@
+package za.co.wethinkcode.robots.world;
+
+/**
+ * Represents a mountain obstacle - a rectengular impassable terrain feature
+ * Robots cannot move through or fire though a Mountain.
+ */
+
+public class Mountain  extends Obstacle {
+
+    public Mountain(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+
+
+}
