3fc9156d36566786b55cae4de744ba775d770768
MpiloMankazana <Mpilomankazana601@gmail.com>
2026-06-20 11:13:34 +0200

    Add getType method to PIT returning PIT

--- src/main/java/za/co/wethinkcode/robots/world/Pit.java
+++ src/main/java/za/co/wethinkcode/robots/world/Pit.java
@@ -10,4 +10,9 @@ public class Pit extends Obstacle {
     public Pit (int x, int y, int width, int height) {
         super(x, y, width, height);
     }
+
+    @Override
+    public String getType() {
+        return "PIT";
+    }
 }
